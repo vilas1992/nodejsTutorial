d6f0eb880db004266db463ab3b5d007eac847035
vilas1992 <vilaslawand1122@gmail.com>
2019-01-09 14:32:01 +0530

    added controller, routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,141 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
+const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
+const ProductsController = require('../controllers/product');
 
-const Product = require('../models/product');
-
-router.get('/', (req, res, next) => {
-    Product.find()
-        .select("name price _id")
-        .exec()
-        .then(docs => {
-            console.log(docs);
-            const response = {
-                count: docs.length,
-                products: docs.map(doc => {
-                    return {
-                        name: doc.name,
-                        price: doc.price,
-                        _id: doc._id,
-                        request: {
-                            type: "GET",
-                            url: "http://localhost:3000/products/" + doc._id
-                        }
-                    }
-                })
-            }
-            res.status(200).json(docs);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
-    
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
 });
 
-router.post('/', (req, res, next) => {
-   
-    const product = new Product({
-        _id: new mongoose.Types.ObjectId,
-        name: req.body.name,
-        price: req.body.price
-    });
-    product.save()
-            .then(result => {
-                console.log(result);
-                res.status(201).json({
-                    message: "Created product successfully",
-                    createdProduct: {
-                        name: result.name,
-                        price: result.price,
-                        _id: result._id,
-                        request: {
-                            type: 'GET',
-                            url: "http://localhost:3000/products/" + result._id
-                        }
-                    }
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                });
-            });
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true);
+    }else{
+        cb(null, false);
+    }
+};
 
-    res.status(200).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
-    });
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 25
+    },
+    fileFilter: fileFilter
 });
 
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    Product.findById(id)
-        .exec()
-        .then(doc =>{
-            console.log(doc);
-            res.status(200).json(doc);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error: err});
-        });        
-   
-});
+router.get('/', ProductsController.products_get_all);
 
-router.patch('/:productId', (req, res, next) => {
-  
-        const id = req.params.productId;
-        const updateOps = {};
-        for(const ops of req.body){
-            updateOps[ops.propName] = ops.value;
-        }
-        Product.update({ _id : id }, { $set: updateOps })
-            .exec()
-            .then(result =>{
-                console.log(result);
-                res.status(200).json({
-                    message: 'Product updated',
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost/products/' +id
-                    }
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                });
-            });
-        
-});
+router.post('/', checkAuth, upload.single('productImage'), ProductsController.products_create_product);
 
-router.delete('/:productId', (req, res, next) => {
-  
-    const id = req.params.productId;
-    Product.remove({_id: id})
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'Product deleted',
-                request: {
-                    type: 'POST',
-                    url: 'http://localhost:3000/products',
-                    body: { name: 'String', price: 'Number'}
-                }
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            })
-        });
-   
-});
+router.get('/:productId', ProductsController.products_get_product);
+
+router.patch('/:productId', ProductsController.products_update_product);
+
+router.delete('/:productId', ProductsController.products_delete);
 
 module.exports = router;
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb://node-shop:'+ encodeURIComponent(process.env.MONGO_ATLAS_PW) +'@cluster0-shard-00-00-ew8lq.mongodb.net:27017,cluster0-shard-00-01-ew8lq.mongodb.net:27017,cluster0-shard-00-02-ew8lq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',{
     useNewUrlParser: true
@@ -13,6 +14,7 @@ mongoose.connect('mongodb://node-shop:'+ encodeURIComponent(process.env.MONGO_AT
 mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -32,6 +34,7 @@ app.use((req, res, next) => {
 // Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
 const error = new Error('Not found');
